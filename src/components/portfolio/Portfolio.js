d69ac9b6f4c9b84b28cf26e98ff5cce524f904d4
babinec <mr.babinec@gmail.com>
2023-12-01 18:26:13 +0100

    fix: portfolio update

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,72 +4,58 @@ import PortfolioBlock from "./PortfolioBlock";
 
 const projects = [
     {
-        name: 'smartweby.sk',
-        url: 'https://smartweby.sk',
-        desc: 'landing page',
-        tech: 'Symfony, React, Bootstrap',
+        name: 'whack-it!',
+        url: 'https://whackit.eu',
+        desc: 'Personal Laravel project',
+        tech: 'Laravel, Tailwind, jQuery, MySQL',
+        source: 'https://github.com/majko96/chirper'
+    },
+    {
+        name: 'radiation-map',
+        url: 'https://radiation-map.netlify.app',
+        desc: 'Simple React app which show actual radiation data',
+        tech: 'React, Bootstrap, Vite, Recoil',
+        source: 'https://github.com/majko96/shmu-vite'
+    },
+    {
+        name: 'poslouchej-radia-admin-panel',
+        url: 'https://radia.moj-dennik.eu',
+        desc: 'Admin panel for management radio stations list for my KODI app',
+        tech: 'Symfony, React, Bootstrap, MySQL',
         source: null
     },
     {
-        name: 'smartweby-cms',
-        url: 'https://allperfectreality.sk',
-        desc: 'cms for real estates',
-        tech: 'Symfony, React, Redux, Bootstrap, microservice',
-        source: null
-    },
-    {
-        name: 'app.trhovereporty.sk',
-        url: 'https://app.trhovereporty.sk',
-        desc: 'spa react - real estate index data',
-        tech: 'React, Mui',
-        source: null
-    },
-    {
-        name: 'trhovereporty.sk',
-        url: 'https://trhovereporty.sk',
-        desc: 'landing page',
-        tech: 'React, Mui',
-        source: null
-    },
-    {
-        name: 'jarell.sk',
-        url: 'https://jarell.sk',
-        desc: 'landing page',
-        tech: 'PHP, javaScript, Bootstrap, jQuery',
-        source: 'https://github.com'
-    },
-    {
-        name: 'moj-dennik.eu',
-        url: 'https://moj-dennik.eu',
-        desc: 'learn PHP and MySQL',
-        tech: 'PHP, javaScript, Bootstrap, jQuery, MySQL',
-        source: 'https://github.com'
-    },
-    {
-        name: 'Poslouchej radia - LMS',
+        name: 'poslouchej-radia-lms-app',
         url: 'https://github.com/majko96/LMS-Poslouchej-radia',
-        desc: 'learn Perl basics',
+        desc: 'Learn Perl basics',
         tech: 'Perl, API requests',
         source: 'https://github.com/majko96/LMS-Poslouchej-radia'
     },
     {
-        name: 'Poslouchej radia - Kodi',
+        name: 'poslouchej-radia-kodi-app',
         url: 'https://github.com/majko96/LMS-Poslouchej-radia',
-        desc: 'learn Python basics',
+        desc: 'Learn Python basics',
         tech: 'Python, XbmcSwift2',
         source: 'https://github.com/Mebox/plugin.audio.poslouchej.radia'
     },
     {
-        name: 'Poslouchej radia - Admin Panel',
-        url: 'https://radia.moj-dennik.eu/',
-        desc: 'admin panel for management radio stations list',
-        tech: 'PHP, javaScript, Bootstrap, MySQL, Symfony',
-        source: 'personal'
+        name: 'jarell.sk',
+        url: 'https://jarell.sk',
+        desc: 'Landing page',
+        tech: 'PHP, javaScript, Bootstrap, jQuery',
+        source: null
     },
     {
-        name: 'Covid-19-SK - Android project',
+        name: 'moj-dennik.eu',
+        url: 'https://moj-dennik.eu',
+        desc: 'Learn PHP and MySQL',
+        tech: 'PHP, javaScript, Bootstrap, jQuery, MySQL',
+        source: null
+    },
+    {
+        name: 'Covid-19-SK-android-app',
         url: 'https://github.com/majko96/covid-19-sk/releases/download/6.0/app-debug.apk',
-        desc: 'learn Android development basics',
+        desc: 'Learn Android development basics',
         tech: 'Java',
         source: 'https://github.com/majko96/covid-19-sk'
     }
